c9ff1ae8fe1db23f7ec2631bf812ddcbf8ec2183
Manish <paramguru45@gmail.com>
2025-07-12 02:40:16 -0700

    added details for forget password in cinfiguration

--- config.py
+++ config.py
@@ -3,9 +3,13 @@ TELEGRAM_BOT_TOKEN = "7598988171:AAGylvhUVZeYBIgYvOXRCsJfChjq8ohoGRs"
 TELEGRAM_CHAT_ID = "6511211034"
 
 
+# config.py
+
 MAIL_SERVER = 'smtp-relay.brevo.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = 'paramguru45@gmail.com'
 MAIL_PASSWORD = 'HcAxvdmhjXCatUry'
+MAIL_DEFAULT_SENDER = 'paramguru45@gmail.com'
+
 
